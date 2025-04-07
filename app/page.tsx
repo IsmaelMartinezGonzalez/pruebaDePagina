0a417d2223401fcd3c33ea1b72612b79a9de5d30
Saluko5 <ismaelmartinezgonzalez05@gmail.com>
2025-04-07 20:47:48 +0200

    primero cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-full  w-full">
       <nav className="flex items-center bg-black max-width h-20">
       <div className="w-20 size-20">
         <img src="/letra.png" alt="" className="size-20"/>
@@ -13,13 +13,28 @@ export default function Home() {
         <p className="px-5">Tienda America</p>
       </div>
       </nav>
+      
+      {/*Contenedor de la parte de las imagenes y eso*/}
+      <div className="flex w-full h-full">
 
-      <div className="flex bg-red-500 w-1/5 text-white">
-        <p>Filtrar eventos</p>
-        <div className="flex justify-items-center">
-          <button>Alfabeticamente</button>
-          <button>Por fecha</button>
+        {/*Primer contendor*/}
+        <div className="flex w-1/5 bg-red-500 text-white h-full justify-items-center max-height flex-col divide-y text-center">
+            <p>Filtrar eventos</p>
+            <button>Alfabeticamente</button>
+            <button>Por fecha</button>
         </div>
+
+        {/*Segundo contendor*/}
+        <div>
+        <div className="bg-white w-60 h-50"> 
+          <p>hola</p>
+          {/*imagen*/}
+          <div className="flex">
+            <img src="/imgprueba1.jpg" alt="" className="w-50 h-30"/>
+          </div>
+        </div>
+        </div>
+
       </div>
     </div>
   );
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -23,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
-      <body className="flex flex-col h-full ">
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
